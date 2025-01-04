f30c6668181e760a4b87f72abe2579c437cbcb16
Agustin Carbajal <magustincarbajal97@gmail.com>
2025-01-04 19:18:02 -0300

    initialize project

---
+++ CursoPelado/FavoritesListVSViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVSViewController.swift
+//  CursoPelado
+//
+//  Created by Agustin Carbajal on 04/01/2025.
+//
+
+import UIKit
+
+class FavoritesListVSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
---
+++ CursoPelado/SearchVCViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class SearchVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+
+}
