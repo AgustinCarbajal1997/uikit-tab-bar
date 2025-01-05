1859b1dba1479ded605020a67b6bbf9b8f741c01
Agustin Carbajal <magustincarbajal97@gmail.com>
2025-01-05 19:45:33 -0300

    search bar

---
+++ CursoPelado/CustomViews/Views/GFEmptyState.swift
@@ -0,0 +1,20 @@
+//
+//  GFEmptyState.swift
+//  CursoPelado
+//
+//  Created by Agustin Carbajal on 05/01/2025.
+//
+
+import UIKit
+
+class GFEmptyState: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
