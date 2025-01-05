7aff4fffa58866475179ed6333df1bced5d45357
Agustin Carbajal <magustincarbajal97@gmail.com>
2025-01-04 21:35:06 -0300

    [chore] navigation controllers

---
+++ CursoPelado/FollowersListVCViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FollowersListVCViewController.swift
+//  CursoPelado
+//
+//  Created by Agustin Carbajal on 04/01/2025.
+//
+
+import UIKit
+
+class FollowersListVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
