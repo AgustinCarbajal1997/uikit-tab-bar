47c7195f898798a1da2f1d216423103fd43dd36f
Agustin Carbajal <magustincarbajal97@gmail.com>
2025-01-07 14:38:51 -0300

    uitableview

---
+++ CursoPelado/CustomViews/Cells/FavoriteCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteCell.swift
+//  CursoPelado
+//
+//  Created by Agustin Carbajal on 07/01/2025.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
